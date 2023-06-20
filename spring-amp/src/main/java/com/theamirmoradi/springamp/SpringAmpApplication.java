5f609ac3e53c543398ca57209ef6428a188e6f80
amirmoradi <theamirmoradi@gmail.com>
2023-06-20 19:52:35 +0330

    add AMQP dependencies

---
+++ spring-amp/src/main/java/com/theamirmoradi/springamp/SpringAmpApplication.java
@@ -0,0 +1,13 @@
+package com.theamirmoradi.springamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAmpApplication.class, args);
+	}
+
+}
---
+++ spring-amp/src/test/java/com/theamirmoradi/springamp/SpringAmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theamirmoradi.springamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
